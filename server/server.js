41c0d8587b69dab8d72bd65e1fc07231ac6479d7
CharlesHumbert <humbertcharless@gmail.com>
2015-11-11 23:28:49 +0100

    delete project example

--- server/server.js
+++ server/server.js
@@ -3,16 +3,4 @@ Meteor.publish('allProjects', function() {
 });
 
 Meteor.startup(function() {
-
-    var project = {
-        dateAdded: new Date(),
-        dateRealized: new Date(),
-        name: "allo",
-        shortDescription: "allo",
-        description: "allo",
-        urlWebsite: "https://allo.com",
-        imgUrl: "allo"
-    };
-
-    Projects.insert(project);
 });
